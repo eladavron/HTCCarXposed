7271904a1e6f0f75927129e08e543cef61ad9f31
Elad Avron <eladavron@gmail.com>
2014-11-02 19:19:11 +0200

    Version 1.9 - Added screen shutdown on exit

---
+++ src/main/java/ambious/htccarxposed/ModuleDeviceAdminReceiver.java
@@ -0,0 +1,16 @@
+package ambious.htccarxposed;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ModuleDeviceAdminReceiver extends BroadcastReceiver {
+    public ModuleDeviceAdminReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        return;
+    }
+
+}
